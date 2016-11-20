07cc507915262ef15c2c18573f467d3b6fcbd39b
Jon Daley <campbelldaley@gmail.com>
2016-11-19 21:36:43 -0500

    refactor smoothing to abstract things away from the routes package

--- models/location.go
+++ models/location.go
@@ -1,6 +1,12 @@
 package models
 
-import(	"github.com/jinzhu/gorm")
+import(
+    "github.com/jinzhu/gorm"
+    "fmt"
+    "strconv"
+    "net/http"
+    "encoding/json"
+)
 
 type Location struct {
 	gorm.Model
@@ -9,3 +15,48 @@ type Location struct {
 	TripID uint
 }
 
+// So yeah this probably isn't the place to be doing any http stuff, but there's too little of it for me to really care about abstracting it away right now
+type MapsResponse struct {
+  SnappedPoints []struct {
+    PlaceID string
+    Location Location
+  }
+}
+
+func SmoothLocationsForTrip(TripID int, rawLocations []Location) (smoothLocations []Location){
+  url := BuildSmoothingURL(rawLocations)
+  response := MapsResponse{}
+  getJson(url, &response)
+
+  for _, smoothLocation := range response.SnappedPoints {
+    smoothLocation := smoothLocation.Location
+    smoothLocation.TripID = uint(TripID)
+    smoothLocations = append(smoothLocations, smoothLocation)
+  }
+  
+  return
+}
+
+func BuildSmoothingURL(oldLocations []Location) string {
+  url := ""
+  if (len(oldLocations) > 0) {
+    url = url + "https://roads.googleapis.com/v1/snapToRoads?key=AIzaSyBS-y6hKLFKiM5yUWIO0AYR5-lrkCZSvp0&path=" // TODO: Probably want to set this as a var on the package
+    url = url + strconv.FormatFloat(oldLocations[0].Latitude, 'f', 6, 64) + "," + strconv.FormatFloat(oldLocations[0].Longitude, 'f', 6, 64)
+    for _, oldLocation := range oldLocations {
+      url = url + "|" + strconv.FormatFloat(oldLocation.Latitude, 'f', 6, 64) + "," + strconv.FormatFloat(oldLocation.Longitude, 'f', 6, 64)
+    }
+  }
+  fmt.Println("url for google maps smoothing:")
+  fmt.Println(url)
+  return url
+}
+
+func getJson(url string, target interface{}) error {  
+  r, err := http.Get(url)
+  if err != nil {
+    return err
+  }
+  defer r.Body.Close()
+
+  return json.NewDecoder(r.Body).Decode(target)
+}
--- router/routes/trips.go
+++ router/routes/trips.go
@@ -6,49 +6,11 @@ import(
        "encoding/json"
        "github.com/gorilla/mux"
        "strconv"
-       "github.com/jinzhu/gorm"
-       "fmt"
-       "lightupon-api/googleMaps"
-       "lightupon-api/redis"
        )
 
-func TripsHandler(w http.ResponseWriter, r *http.Request) {
+func TripsHandler(w http.ResponseWriter, r *http.Request) {  
   lat, lon := GetUserLocationFromRequest(r)
-  trips := []models.Trip{}
-  models.DB.Preload("User").Preload("Scenes", func(DB *gorm.DB) *gorm.DB {
-    return DB.Order("Scenes.scene_order ASC") // Preload and order scenes for the map view
-  }).Order("((latitude - " + lat + ")^2.0 + ((longitude - " + lon + ")* cos(latitude / 57.3))^2.0) asc;").Find(&trips)
-
-  // TODO: this should probably be abstracted in some way
-  // Now attach locations from redis
-  for i, trip := range trips {
-    // Ok what we're going to do here is attempt to pull smooth locations out of Redis. If we're not successful, then attempt to put them in there and try pulling them out again. 
-    smoothLocations := redis.GetSmoothedLocationsFromRedis(int(trip.ID))
-    if (len(smoothLocations) > 0) {
-      trips[i].Locations = smoothLocations
-      fmt.Println("INFO: (Attempt 1) Found some smooth locations in Redis for TripID = " + strconv.Itoa(int(trips[i].ID)) + ". Total points found was: " + strconv.Itoa(len(smoothLocations)))
-    } else {
-      fmt.Println("ERROR: (Attempt 1) Didn't find any smooth locations in Redis for TripID = " + strconv.Itoa(int(trips[i].ID)))
-
-      // see if there are actually any raw locations to be smoothed
-      rawLocations := []models.Location{}
-      models.DB.Where("trip_id = ?", trips[i].ID).Find(&rawLocations)
-      if (len(rawLocations) > 0) {
-        smoothLocationsFromGoogle := googleMaps.SmoothTrip(int(trips[i].ID), rawLocations)
-        // Now let's throw the smoothed trip up into the DB
-        redis.SaveSmoothedLocationsToRedis(int(trips[i].ID), smoothLocationsFromGoogle) //comment this out while testing the GET below
-        smoothLocations := redis.GetSmoothedLocationsFromRedis(int(trips[i].ID))
-        if (len(smoothLocations) > 0) {
-          trips[i].Locations = smoothLocations
-          fmt.Println("INFO: (Attempt 2) Found some smooth locations in Redis for TripID = " + strconv.Itoa(int(trips[i].ID)) + ". Total points found was: " + strconv.Itoa(len(smoothLocations)))
-        } else {
-          fmt.Println("ERROR: (Attempt 2) Didn't find any smooth locations in Redis for TripID = " + strconv.Itoa(int(trips[i].ID)))
-        }
-      } else {
-        fmt.Println("INFO: Didn't find any raw locations in DB for TripID = " + strconv.Itoa(int(trips[i].ID)))
-      }
-    }
-  }
+  trips := models.GetTripsNearLocation(lat, lon)
 
   json.NewEncoder(w).Encode(trips)
 }
