0ba86bb52e7fff4a02f6ba047491a6e78a930ba0
Jon Daley <campbelldaley@gmail.com>
2016-11-19 21:36:43 -0500

    refactor smoothing to abstract things away from the routes package

--- models/trip.go
+++ models/trip.go
@@ -2,6 +2,10 @@ package models
 
 import(
       "github.com/jinzhu/gorm"
+      "lightupon-api/redis"
+      "strconv"
+       "encoding/json"
+       "fmt"
       )
 
 type Trip struct {
@@ -22,3 +26,73 @@ type Trip struct {
 func (t *Trip) PutLocations(locations []Location) {
   DB.Model(&t).Association("Locations").Replace(locations)
 }
+
+func GetTripsNearLocation(lat string, lon string) (trips []Trip) {
+  DB.Preload("User").Preload("Scenes", func(DB *gorm.DB) *gorm.DB {
+    return DB.Order("Scenes.scene_order ASC") // Preload and order scenes for the map view
+  }).Order("((latitude - " + lat + ")^2.0 + ((longitude - " + lon + ")* cos(latitude / 57.3))^2.0) asc;").Find(&trips)
+
+  for i, _ := range trips {
+    locations := GetLocationsForTrip(trips[i].ID)
+    trips[i].Locations = locations
+  }
+
+  return
+}
+
+func GetLocationsForTrip(tripID uint) (locations []Location){
+  locations = GetSmoothedLocationsFromRedis(int(tripID))
+
+  // If we find something in redis then return
+  if (len(locations) > 0) {
+    fmt.Println("INFO: Found some smooth locations in Redis for TripID = " + strconv.Itoa(int(tripID)) + ". Total points found was: " + strconv.Itoa(len(locations)))
+    return
+  } else {
+    fmt.Println("INFO: Didn't find any smooth locations in Redis for TripID = " + strconv.Itoa(int(tripID)))
+  }
+
+  // Try to pull the locations out of the DB. If we find nothing then we're SOL, so return. If we found something, we might need it later if we fail to smooth
+  rawLocations := []Location{}; DB.Where("trip_id = ?", tripID).Find(&rawLocations)  // If we decide later that we never want to display raw trips, then we should just reflect onto 'locations' here
+  if (len(rawLocations) == 0) {
+    fmt.Println("INFO: Didn't find any raw locations in DB for TripID = " + strconv.Itoa(int(tripID)))
+    return
+  } 
+
+  if (!AllowSmoothingRequestForTrip(tripID)) { 
+    fmt.Println("INFO: Smoothing request rate limited for TripID = " + strconv.Itoa(int(tripID)))
+    return
+  }
+
+  // TODO: fix import cycle where googMaps package is using models - so we can uncomment the line below
+  locations = SmoothLocationsForTrip(int(tripID), rawLocations)
+
+  if (len(locations) == 0) { 
+    fmt.Println("ERROR: Didn't get any smooth locations back from Google for TripID = " + strconv.Itoa(int(tripID)))
+    return rawLocations // ok if we've tried all that stuff and nothing has worked, just return the raw locations
+  } else {
+    // AHA! we got some smoothness back from google, save that shit in redis and also return it
+    fmt.Println("INFO: We got some smooth locations back from Google for TripID = " + strconv.Itoa(int(tripID)))
+    SaveSmoothedLocationsToRedis(tripID, locations)
+    return
+  }
+}
+
+func SaveSmoothedLocationsToRedis(tripID uint, locations []Location) {
+  // TODO: Use something more descriptive as the key when saving to redis
+  value, _ := json.Marshal(locations) 
+  key := "locations_" + strconv.Itoa(int(tripID))
+  redis.SaveByteArrayToRedis(key, value) //comment this out while testing the GET below
+}
+
+func AllowSmoothingRequestForTrip(tripID uint) bool {
+  // TODO: add rate limiting for smoothing requests on a per-trip basis
+  return true
+}
+
+func GetSmoothedLocationsFromRedis(TripID int) (smoothLocations []Location) {
+  key := strconv.Itoa(TripID)
+  redisResponseBytes := redis.GetByteArrayFromRedis(key)
+  _ = json.Unmarshal(redisResponseBytes, &smoothLocations)
+  return
+}
+
--- redis/main.go
+++ redis/main.go
@@ -1,35 +1,21 @@
 package redis
 
 import(
-       "lightupon-api/models"
-       "encoding/json"
-       "strconv"
        "github.com/garyburd/redigo/redis"
        )
 
-
-func GetSmoothedLocationsFromRedis(TripID int) []models.Location {
-
+func GetByteArrayFromRedis(key string) []byte {
   client, err := redis.Dial("tcp", ":6379")
-    if err != nil {
-        panic(err)
-    }
-    defer client.Close()
-
-    key := strconv.Itoa(TripID)
-    redisResponse, _ := redis.String(client.Do("GET", key))
-    redisResponseBytes := []byte(redisResponse)
-
-    locations := []models.Location{}
-
-    _ = json.Unmarshal(redisResponseBytes, &locations)
-    
-    return locations
+  if err != nil {
+      panic(err)
+  }
+  defer client.Close()
+  redisResponse, _ := redis.String(client.Do("GET", key))
+  return []byte(redisResponse)
 }
 
-func SaveSmoothedLocationsToRedis(TripID int, smoothedLocations []models.Location) {
-  key := strconv.Itoa(TripID)
-  value, _ := json.Marshal(smoothedLocations)
+
+func SaveByteArrayToRedis(key string, value []byte) {
   client, err := redis.Dial("tcp", ":6379")
   if err != nil {
       panic(err)
