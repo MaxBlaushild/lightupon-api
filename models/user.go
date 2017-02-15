2f8403ab36d5ffea73680dafad35d2224443f7a8
Max Blaushild <blaushmild@gmail.com>
2017-02-14 21:46:37 -0500

    checking to see if this is the fuckup

--- models/user.go
+++ models/user.go
@@ -43,7 +43,7 @@ func (u *User) BeforeCreate() (err error) {
 }
 
 func (u *User) AddTrip(trip *Trip) (err error) {
-  trip.Scenes[0].UserID = u.ID
+  // trip.Scenes[0].UserID = u.ID
   err = DB.Model(&u).Association("Trips").Append(&trip).Error
   return
 }
