2a4909bb2d1462cfca70db9f68c33b7468445daa
Jon Daley <campbelldaley@gmail.com>
2017-03-09 19:59:37 -0500

    throw some Ds

--- python/LightuponWebStuff.py
+++ python/LightuponWebStuff.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import random
 
 def postTrip(title, description, imageURL, latitude, longitude):
 	# DEV - don't use my token tho ya punk bitch
@@ -11,3 +12,24 @@ def postTrip(title, description, imageURL, latitude, longitude):
 	cards = [{'Caption': description, 'ImageURL' : imageURL}]
 	payload = {'Latitude': latitude, 'Longitude' : longitude, 'Name': title, 'Route': 'aksdjhfkasjhdf', 'Street': '12', 'BackgroundURL':imageURL, 'Cards':cards}
 	r = requests.post(url, data=json.dumps(payload), headers=headers)
+
+def throwSomeDs(numberOfDs, title, description, imageURL):
+	for i in range(numberOfDs):
+		postTripWithRandomLocation(title, description, imageURL)
+
+def postTripWithRandomLocation(title, description, imageURL):
+	latitude = randomLocation()[0]
+	longitude = randomLocation()[1]
+	postTrip(title, description, imageURL, latitude, longitude)
+
+def randomLocation():
+	# smaller area
+	lat = random.uniform(42.332856, 42.342373)
+	lon = random.uniform(-71.090436, -71.065889)
+	# small area
+	# lat = random.uniform(42.326854, 42.368061)
+	# lon = random.uniform(-71.110049, -71.027308)
+	# large area
+	# lat = random.uniform(42.289632, 42.446392)
+	# lon = random.uniform(-71.268320, -70.878992)
+	return [lat, lon]
