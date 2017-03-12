cf7a3585c0895b045823e2785a2dc96fb6fb2698
Jon Daley <campbelldaley@gmail.com>
2017-03-12 17:22:15 -0400

    shit tight no slack

--- models/trip.go
+++ models/trip.go
@@ -79,12 +79,12 @@ func GetTripsNearLocation(lat string, lon string, userID uint) (trips []Trip) {
   DB.Preload("User").Preload("Scenes.Cards").Find(&trips)
   trips = trips[:20] // Limit() appears to not work in GORM, so heres a hack
 
-  // scene := trips[0].Scenes[0]
+  scene := trips[0].Scenes[0]
 
   for i, _ := range trips {
     // experimental inventory stuff
     if len(trips[i].Scenes) > 0 {
-      // trips[i].Scenes = append(trips[i].Scenes, scene)
+      trips[i].Scenes = append(trips[i].Scenes, scene)
       // fmt.Println("trips[i].Scenes[0].Name")
       // fmt.Println(trips[i].Scenes[0].Name)
       if trips[i].Scenes[0].Name == "inventory" {
@@ -246,4 +246,3 @@ func (trip *Trip) LoadLikeStuff(userID uint) {
   // Find out the total number of likes for the trip
   trip.TotalLikes = GetTotalLikesForTrip(trip.ID)
 }
-
