d20e235b740edda82382f5ccb24d134ef66fdcfb
Jonathan Daley <campbelldaley@gmail.com>
2019-02-25 13:59:01 -0500

    fixed bug where GetNearbyPostsAndTryToDiscoverThem was trying to access the user location from the request incorrectly; for now we'll spew all the scenes out for debugging, to make sure we're getting the right ones

--- router/routes/posts.go
+++ router/routes/posts.go
@@ -7,6 +7,7 @@ import(
        "github.com/gorilla/mux"
        // "strconv"
        "fmt"
+       "github.com/davecgh/go-spew/spew"
 )
 
 func CreatePost(w http.ResponseWriter, r *http.Request) {
@@ -55,18 +56,14 @@ func CompletePostHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetNearbyPostsAndTryToDiscoverThem(w http.ResponseWriter, r *http.Request) {
-  decoder := json.NewDecoder(r.Body)
-
   lat, lon := GetLocationFromRequest(r)
   user := GetUserFromRequest(r)
   radius := GetStringFromRequest(r, "radius", "5000")
 
-  err := decoder.Decode(&user.Location); if err != nil {
-    respondWithBadRequest(w, "The location sent was bunk.")
-    return
-  }
-
   posts, err := models.GetNearbyPostsAndTryToDiscoverThem(user, lat, lon, radius, 20)
+
+  // For now, let's spew all the scenes out for debugging, to make sure we're getting the right ones.
+  spew.Dump(posts)
 
   if err != nil {
     fmt.Println(err)
